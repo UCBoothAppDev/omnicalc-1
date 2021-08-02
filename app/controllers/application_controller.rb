d3961c24bbd67aafe2ceeec486ad73cee6b735f9
Canderson2120 <connor.anderson@chicagobooth.edu>
2021-08-02 19:13:28 +0000

    OmniCalc Progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,38 @@
 class ApplicationController < ActionController::Base
 
+    def blank_square_form
+
+        render({ :template => "calculation_templates/square_form.html.erb"})
+    end 
+
+    def calculate_square
+            # params = {"elephant"=>"42"}
+
+    @num = params.fetch("elephant").to_f
+    @square_of_num = @num ** 2
+
+        render({ :template => "calculation_templates/square_results.html.erb"})
+    end 
+
+    def calculate_random 
+
+        @lower = params.fetch("user_min").to_f
+        @upper = params.fetch("user_max").to_f
+        @result = rand(@lower..@upper)
+
+        render({ :template => "calculation_templates/rand_results.html.erb" })
+    end
+
+    def rand_with_form
+
+        render({ :template => "calculation_templates/rand_with_form.html.erb"})
+
+    end
+
+    def new_payment
+
+        render({ :template => "calculation_templates/new_payment.html.erb"})
+
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
 
+    get("/square/new", { :controller => "application", :action => "blank_square_form" })
+
+    get("/payment/new", { :controller => "application", :action => "new_payment" })
+  
+    get("/square/results", { :controller => "application", :action => "calculate_square" })
+    
+    get("/random/results", { :controller => "application", :action => "calculate_random"})
+
+    get("/random/new", { :controller => "application", :action => "rand_with_form"})
+
 end
